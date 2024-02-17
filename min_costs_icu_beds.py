88e6229f5ed1a6e2594bac3f5c905d232243a818
Lara <laradicp@gmail.com>
2024-02-17 14:52:35 -0300

    add instance and fixes but still with bugs

--- min_costs_icu_beds.py
+++ min_costs_icu_beds.py
@@ -1,30 +1,29 @@
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
-import numpy as np
 
 class Data:
   def __init__(self, filename):
     with open(filename) as file_object:
-      self.F = range(int(file_object.readline())) # F: set of facilities
+      self.F = list(range(int(file_object.readline()))) # F: set of facilities
+      self.K = [int(n) for n in file_object.readline().split()] # K: built facilities
       n_req = 0
       req = []
       for n in file_object.readline().split():
+        req.append(list(range(n_req, n_req + int(n))))
         n_req += int(n)
-        req.append(range(n_req))
       self.E, self.I, self.S = req # E: set of equipments; I: set of infrastructure; S: set of staff
-      self.K = [int(n) for n in file_object.readline().split()] # K: built facilities
-      self.d = int(file_object.read_line()) # d: demand of ICU beds
+      self.d = int(file_object.readline()) # d: demand of ICU beds
       self.c = [] # c: cost of building facilities
-      for n in file_object.readline().split():
-        if n in self.K:
+      for i, n in enumerate(file_object.readline().split()):
+        if i in self.K:
           self.c.append(0)
         else:
           self.c.append(float(n))
-      self.l = [int(n) for n in file_object.readline().split()] # l: lower bound of ICU beds in each facility
+      self.l = [int(n) for n in file_object.readline().split()] # l: lower bound of ICU beds in each facility, if built
       self.u = [int(n) for n in file_object.readline().split()] # u: upper bound of ICU beds in each facility
       self.p = [float(n) for n in file_object.readline().split()] # p: price of each requirement
       self.r = [float(n) for n in file_object.readline().split()] # r: repair price of each requirement (equipments and infrastructure)
-      self.n = [int(n) for n in file_object.readline().split()] # n: necessary rate of each requirement per ICU bed
+      self.n = [float(n) for n in file_object.readline().split()] # n: necessary rate of each requirement per ICU bed
       self.a = [] # a: availability of each requirement in each facility
       for _ in self.F:
         self.a.append([int(n) for n in file_object.readline().split()])
@@ -32,10 +31,10 @@ class Data:
       for _ in self.F:
         self.m.append([int(n) for n in file_object.readline().split()])
       self.t = [] # t: transfer cost of each requirement among hospitals
-      for _ in np.concatenate(self.E, self.S):
+      for _ in (self.E + self.S):
         costs_for_req_j = []
         for _ in self.F:
-          self.costs_for_req_j.append([int(n) for n in file_object.readline().split()])
+          costs_for_req_j.append([int(n) for n in file_object.readline().split()])
         self.t.append(costs_for_req_j)
 
 class Model:
@@ -48,14 +47,14 @@ class Model:
     model.K = data.K
     model.x = pyo.Var(model.F, within=pyo.Integers) # x: number of ICU beds in each facility
     model.y = pyo.Var(model.F, within=pyo.Binary) # y: whether each facility is built or not
-    model.z = pyo.Var(model.F, np.concatenate(model.E, model.I, model.S), within=pyo.Integers) # z: number of each requirement acquired by each facility
-    model.w = pyo.Var(model.F, np.concatenate(model.E, model.I), within=pyo.Integers) # w: number of each requirement repaired in each facility
-    model.v = pyo.Var(np.concatenate(model.E, model.S), model.F, model.F, within=pyo.Integers) # v: number of each requirement transferred from each facility to each facility
+    model.z = pyo.Var(model.F, (model.E + model.I + model.S), within=pyo.Integers) # z: number of each requirement acquired by each facility
+    model.w = pyo.Var(model.F, (model.E + model.I), within=pyo.Integers) # w: number of each requirement repaired in each facility
+    model.v = pyo.Var((model.E + model.S), model.F, model.F, within=pyo.Integers) # v: number of each requirement transferred from each facility to each facility
     # Objective function
     model.objective = pyo.Objective(expr=sum(data.c[i]*model.y[i] + sum(data.p[j]*model.z[i, j]
-      for j in np.concatenate(model.E, model.I, model.S)) + sum(data.m[j]*model.w[i, j]
-      for j in np.concatenate(model.E, model.I)) + sum(sum(data.t[j][i][l]*model.v[j, i, l]
-      for l in model.F if l != i) for j in np.concatenate(model.E, model.S)) for i in model.F),
+      for j in (model.E + model.I + model.S)) + sum(data.m[i][j]*model.w[i, j]
+      for j in (model.E + model.I)) + sum(sum(data.t[j][i][l]*model.v[j, i, l]
+      for l in model.F if l != i) for j in (model.E + model.S)) for i in model.F),
       sense=pyo.minimize)
     # Constraints
     model.demand_constraint = pyo.Constraint(expr=sum(model.x[i] for i in model.F) >= data.d)
@@ -76,7 +75,7 @@ class Model:
           for l in model.F if l != i) >= data.n[j]*model.x[i])
     model.repair_constraint = pyo.ConstraintList()
     for i in model.F:
-      for j in np.concatenate(model.E, model.I):
+      for j in (model.E + model.I):
         model.repair_constraint.add(model.w[i, j] <= data.m[i][j])
     model.bed_limit_constraint = pyo.ConstraintList()
     for i in model.F:
@@ -93,4 +92,4 @@ class Model:
     results = opt.solve(model, tee=True)
     results.write()
 
-model = Model(Data('../instances/mock.txt'))
+model = Model(Data('instances/mock.txt'))
